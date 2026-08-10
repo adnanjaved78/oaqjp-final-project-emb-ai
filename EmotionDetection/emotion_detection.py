d86ad5295eeadd42d834ab3d3c2b558baa016577
Adnan Javed <adnan.doc@gmail.com>
2026-08-10 16:07:56 -0400

    Packaging EmotionDetection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload =  { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json= payload, headers= headers, timeout=5)
+
+    response_dictionary = json.loads(response.text)
+    emotions_dictionary = response_dictionary["emotionPredictions"][0]["emotion"]
+
+    emotions_dictionary["dominant_emotion"] = get_dominant_emotion(emotions_dictionary)
+
+    return emotions_dictionary
+
+def get_dominant_emotion(emotions):
+    dominant = None
+    highest = -1.0
+    for emotion, score in emotions.items():
+        if score > highest:
+            highest = score
+            dominant = emotion
+    return dominant
